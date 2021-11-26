32e5ba0d1e2379be17dfac6bfa81ea681153f64e
afeiship <1290657123@qq.com>
2021-11-26 12:49:45 +0800

    feat: ts-jest worked

---
+++ src/2021-11/jest-react/src/__tests__/components/checkbox-group.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { CheckboxGroup} from '../../components/checkbox-group';
+
+describe('<LoginForm />', () => {
+  test('should display a blank login form, with remember me checked by default', async () => {
+    // ???
+  });
+});
--- src/2021-11/jest-react/src/components/checkbox-group/index.tsx
+++ src/2021-11/jest-react/src/components/checkbox-group/index.tsx
@@ -2,5 +2,8 @@ import React from 'react';
 
 export const CheckboxGroup = (props: any) => {
   console.log('props', props);
-  return <div>checkbox-group</div>;
+  return <div>
+    <strong>checkbox-group</strong>
+    <input type='checkbox' />
+  </div>;
 };
